1448866fd918f7b7d164258f644a2da832eb924a
Marc Rodriguez <marcrodrimartin@gmail.com>
2024-06-01 12:16:48 +0200

    Fix error on api logic with some materials

--- routes/processesApi.js
+++ routes/processesApi.js
@@ -29,7 +29,7 @@ router.get('/:material_name', async (req, res) => {
             }
         });
 
-        const data = await getProcess(process_, material.id, [{ 'material_id': material.id, 'material_name': material.material_name, 'extras': [] }])
+        const data = await getProcess(process_, material.id, [{ 'material_id': material.id, 'material_name': material.material_name, 'extras': [] }], [material.id])
             .then(data => {
 
                 return printSequence(data)
@@ -42,33 +42,38 @@ router.get('/:material_name', async (req, res) => {
 });
 module.exports = router;
 
-async function getProcess(process, original, currentPath, paths = []) {
+async function getProcess(process, original, currentPath, processedMaterials = [], paths = []) {
     for (let element of process) {
-        const material = await Materials.findByPk(element.input_material)
-        const machine = await Machines.findByPk(element.machine_id)
+        const material = await Materials.findByPk(element.input_material);
+        const machine = await Machines.findByPk(element.machine_id);
 
-
-
-        const newPath = [...currentPath, { 'id': element.id, 'material_id': element.input_material, 'material_name': material.material_name, 'machine_id': element.machine_id, 'machine_name': machine.machine_name, 'extras': [] }];
+        const newPath = [...currentPath, {
+            'id': element.id,
+            'material_id': element.input_material,
+            'material_name': material.material_name,
+            'machine_id': element.machine_id,
+            'machine_name': machine.machine_name,
+            'extras': []
+        }];
 
         const extras = await Extras.findAll({
             where: {
                 process_id: element.id,
             }
-
-        })
-
+        });
 
         if (extras.length > 0) {
-            for (let extra in extras) {
-                const materialExtra = await Materials.findByPk(extras[extra].material_id);
-                newPath[newPath.length - 1][`extras`].push({ 'material_id': extras[extra].material_id, 'material_name': materialExtra.material_name, 'quantity': extras[extra].extra_quantity });
-
+            for (let extra of extras) {
+                const materialExtra = await Materials.findByPk(extra.material_id);
+                newPath[newPath.length - 1]['extras'].push({
+                    'material_id': extra.material_id,
+                    'material_name': materialExtra.material_name,
+                    'quantity': extra.extra_quantity
+                });
             }
         }
+        if (!processedMaterials.includes(element.input_material)) {
 
-
-        if (element.input_material !== original) {
             const materialProcess = await Processes.findAll({
                 where: {
                     output_material: element.input_material
@@ -78,22 +83,22 @@ async function getProcess(process, original, currentPath, paths = []) {
             if (materialProcess.length !== 0) {
                 if (materialProcess.length > 1) {
                     for (let subProcess of materialProcess) {
-                        await getProcess([subProcess], original, newPath, paths);
+                        await getProcess([subProcess], original, newPath, [...processedMaterials, element.input_material], paths);
                     }
                 } else {
-                    await getProcess(materialProcess, original, newPath, paths);
+                    await getProcess(materialProcess, original, newPath, [...processedMaterials, element.input_material], paths);
                 }
             } else {
                 paths.push(newPath);
             }
+
         } else {
             paths.push(newPath);
-            newPath.pop();
+            newPath.pop()
         }
     }
     return paths;
 }
-
 function printSequence(sequence) {
     let printSequence = [];
 
@@ -111,9 +116,9 @@ function printSequence(sequence) {
 
             if (sequence[element][subProcess].extras.length > 0) {
                 for (let extra in sequence[element][subProcess].extras) {
-                    sequenceProcessString += ` [Extra ${parseInt(extra) + 1}: ${sequence[element][subProcess].extras[extra].material_name} | ` ;
-                    
-                    if([93, 94].includes(sequence[element][subProcess].extras[extra].material_id)){
+                    sequenceProcessString += ` [Extra ${parseInt(extra) + 1}: ${sequence[element][subProcess].extras[extra].material_name} | `;
+
+                    if ([93, 94].includes(sequence[element][subProcess].extras[extra].material_id)) {
                         sequenceProcessString += `Cantidad: ${sequence[element][subProcess].extras[extra].quantity} mb]`;
                     }
                     else sequenceProcessString += `Cantidad: ${sequence[element][subProcess].extras[extra].quantity}]`;
